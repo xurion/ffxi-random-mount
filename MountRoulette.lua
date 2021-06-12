a20531c4ed122d9801f6a1dc0a978e59dbc2fa67
Dean James <xurion@hotmail.com>
2021-06-12 16:07:03 +0100

    Add missing KI detection.

--- MountRoulette.lua
+++ MountRoulette.lua
@@ -49,7 +49,10 @@ function update_allowed_mounts()
 
     for _, id in ipairs(kis) do
         local ki = resources.key_items[id]
-        if ki.category == 'Mounts' and ki.name ~= "trainer's whistle" then -- Don't care about the quest KI
+        if not ki then
+            print('KI not found in resources', id);
+        end
+        if ki and ki.category == 'Mounts' and ki.name ~= "trainer's whistle" then -- Don't care about the quest KI
             local mount_index = possible_mounts:find(function(possible_mount)
                 return windower.wc_match(ki.name:lower(), '♪' .. possible_mount .. '*')
             end)
